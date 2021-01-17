3b4d290ce091a838676c41601aec50a2f01bc773
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2021-01-17 16:19:33 -0600

    dotenv (variables de entorno)

---
+++ database/config.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const dbConnection = async() => {
+
+    try {
+        
+        await mongoose.connect(process.env.DB_CNN, 
+                        {
+                            useNewUrlParser: true,
+                            useUnifiedTopology: true
+                        });
+
+                        console.log('DB Online');
+      
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error al iniciar la base de datos')
+        
+    }
+}
+
+module.exports = { dbConnection }
