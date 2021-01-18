5a790908a8509458040fa35e930d04781a1a164b
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2021-01-17 20:20:03 -0600

    get y post de usuarios (clase 107)

---
+++ controllers/usuarios.js
@@ -0,0 +1,30 @@
+const Usuario = require('../models/usuario');
+
+const getUsuarios = async (req, res) => {
+
+    const usuario = await Usuario.find({}, 'nombre email role google');
+
+    res.json({
+        ok: true,
+        usuario
+    });
+}
+
+const crearUsuario = async (req, res) => {
+
+    const { email, password, nombre } = req.body;
+
+    const usuario = new Usuario( req.body );
+
+    await usuario.save();
+
+    res.json({
+        ok: true,
+        usuario
+    });
+}
+
+module.exports = { 
+    getUsuarios,
+    crearUsuario,
+}
--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 require('dotenv').config();
 
 const express = require('express');
-var cors = require('cors')
+var cors = require('cors');
 
 
-const { dbConnection } = require('./database/config')
+const { dbConnection } = require('./database/config');
 
 // Crear el servidor express
 const app = express();
 
 
 // Configurar CORS
-app.use(cors())
+app.use(cors());
 
+
+// Lectura y parseo del body
+app.use( express.json() );
 
 
 // Base de datos
 dbConnection();
 
 //Rutas
-app.get('/', (req, res)=> {
+app.use('/api/usuarios', require('./routes/usuarios'));
 
-    res.json({
-        ok: true,
-        msg: 'Hola mundo'
-    })
-
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
